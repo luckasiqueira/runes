22267c0efadd1ee7697f3af8326f997443469dac
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-03-06 16:03:24 -0300

    Added endpoints for POSTS (draws)

--- cmd/runes/controller/dle-endpoints.go
+++ cmd/runes/controller/dle-endpoints.go
@@ -1,1 +1,15 @@
 package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"runes/cmd/runes/game-modes/dle"
+	"strings"
+)
+
+/*
+DLEDraws collects user's shots to draw var, which will be converted as uppercase
+*/
+func DLEDraws(context *gin.Context) {
+	draw := strings.ToUpper(context.PostForm("draw"))
+	dle.PlayDLE(context, draw)
+}
