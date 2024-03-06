14ece5403c134ec81763374907fd5aad94805b44
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-03-06 17:42:02 -0300

    Update play.go

--- cmd/runes/game-modes/dle/play.go
+++ cmd/runes/game-modes/dle/play.go
@@ -1,7 +1,6 @@
 package dle
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/robfig/cron/v3"
 	"runes/cmd/runes/database"
@@ -10,17 +9,21 @@ import (
 var dailyChampion *database.ChampionLOL
 
 /*
- */
+PlayDLE is where game starts, implementing some initial evaluations
+gameID collects the gameID param from the URL, to identify from the URL what game mode is user playing
+dailyDraw is executed if game mode is "guess" and user must guess champion set on pointer dailyChampion
+*/
 func PlayDLE(context *gin.Context, draw string) {
 	gameID := context.Param("gameID")
+	var championID int
 	var champion database.ChampionLOL
+	table := database.SetTable(context)
 	if context.Request.URL.Path == "/try/guess/"+gameID {
 		dailyDraw()
-		champion = *dailyChampion
+		champion = *(dailyChampion)
 	} else if context.Request.URL.Path == "/try/mayhem/"+gameID {
-
+		championID = database.CheckGameChampion(gameID, table)
 	}
-	fmt.Println(champion)
 }
 
 /*
