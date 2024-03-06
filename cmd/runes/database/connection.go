642f2a4c5e4e9b0fab8d9fc8cc100bd43938a496
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-03-06 17:42:16 -0300

    Added SetTable func

--- cmd/runes/database/connection.go
+++ cmd/runes/database/connection.go
@@ -3,6 +3,7 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	"log"
@@ -25,4 +26,23 @@ func Connect() *sql.DB {
 	db, err := sql.Open("mysql", data)
 	db.SetConnMaxLifetime(time.Minute * 5)
 	return db
+}
+
+/*
+SetTable checks the given URL Path to identify game mode.
+When game mode is identified, atributes a value set on .env file to table var, which is returned
+*/
+func SetTable(context *gin.Context) string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Connect() -> error while loading .env file.")
+	}
+	var table string
+	gameID := context.Param("gameID")
+	if context.Request.URL.Path == "/play/guess/"+gameID || context.Request.URL.Path == "/try/guess/"+gameID {
+		table = os.Getenv("TB_GUESS")
+	} else if context.Request.URL.Path == "/play/mayhem/"+gameID || context.Request.URL.Path == "/try/mayhem/"+gameID {
+		table = os.Getenv("TB_MAYHEM")
+	}
+	return table
 }
