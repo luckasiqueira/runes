b4e4d7af8b51758c9f3accc177e0c28bd993919d
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-03-06 17:51:41 -0300

    Renamed dailyDraw() to draftDailyChampion()

--- cmd/runes/game-modes/dle/play.go
+++ cmd/runes/game-modes/dle/play.go
@@ -1,6 +1,7 @@
 package dle
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/robfig/cron/v3"
 	"runes/cmd/runes/database"
@@ -24,13 +25,14 @@ func PlayDLE(context *gin.Context, draw string) {
 	} else if context.Request.URL.Path == "/try/mayhem/"+gameID {
 		championID = database.CheckGameChampion(gameID, table)
 	}
+	fmt.Println(championID, champion)
 }
 
 /*
 dailyDraw runs every 00:00 (here, set as 12), when it Draws a new champion from DB and saves onto dailyChampion pointer
 This pointer will be used to compare user shots fast, since no DB comparision will be needed
 */
-func dailyDraw() {
+func draftDailyChampion() {
 	cronCycle := "* * * * *"
 	job := cron.New()
 	job.AddFunc(cronCycle, func() {
