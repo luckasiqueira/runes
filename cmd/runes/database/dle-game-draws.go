7787ca6a15f42f52e29dbc60899d8a943f15fd71
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-03-12 17:35:46 -0300

    Isolating table definition to a proper func

--- cmd/runes/database/dle-game-draws.go
+++ cmd/runes/database/dle-game-draws.go
@@ -7,13 +7,10 @@ import (
 	"os"
 )
 
-/*
-SaveDraw connects to DB and place the given championID on it, in order to save all shots for each game
-*/
-func SaveDraw(gameID, playingMode string, championID int) {
+func setTable(playingMode string) string {
 	errr := godotenv.Load()
 	if errr != nil {
-		log.Fatal("SaveDraw() -> error while loading .env file")
+		log.Fatal("setTable() -> error while loading .env file")
 	}
 	var table string
 	if playingMode == "Guess" {
@@ -21,10 +18,22 @@ func SaveDraw(gameID, playingMode string, championID int) {
 	} else if playingMode == "Mayhem" {
 		table = os.Getenv("TB_MAYHEM_DRAWS")
 	}
+	return table
+}
+
+/*
+SaveDraw connects to DB and place the given championID on it, in order to save all shots for each game
+*/
+func SaveDraw(gameID, playingMode string, championID int) {
+	table := setTable(playingMode)
 	db := Connect()
 	_, err := db.Exec(fmt.Sprintf("INSERT INTO `%s` (`id`, `draw`, `gameID`) VALUES (NULL, '%d', '%s');", table, championID, gameID))
 	if err != nil {
 		log.Fatal("SaveDraw() -> error while saving draw on BD")
 	}
 	defer db.Close()
+}
+
+func CheckDraws() {
+
 }
