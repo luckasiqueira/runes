6b97a921b52345abfbf6c456ed41fae64b74158e
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-03-12 23:31:58 -0300

    Renamed Guess func as DLEs

--- cmd/runes/controller/mode-dles.go
+++ cmd/runes/controller/mode-dles.go
@@ -1,24 +1,39 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"runes/cmd/runes/database"
+	"runes/cmd/runes/game-modes/dle"
 )
 
 /*
 Guess controller sets the entrypoint of Guess Game Mode, which will save gameID on database and render page HTML
 */
-func Guess(context *gin.Context) {
+func DLEs(context *gin.Context) {
 	gameID := context.Param("gameID")
 	database.SaveGame(context, gameID)
 	var playingMode string
+	var championID int
+
+	table := database.SetTable(context)
 	if context.Request.URL.Path == "/play/guess/"+gameID {
 		playingMode = "guess"
+		championID = (*dle.DailyChampion).ID
 	} else if context.Request.URL.Path == "/play/mayhem/"+gameID {
 		playingMode = "mayhem"
+		championID = database.CheckGameChampion(gameID, table)
 	}
+	champion := dle.FindChampion(championID)
 	gameDraws := database.CheckDraws(gameID, playingMode)
+	for i := range gameDraws {
+		var gameDraw database.Draws
+		gameDraw.Champion = gameDraws[i].Champion
+		gameDraw = dle.Compare(gameDraw.Champion, champion, gameDraw)
+		gameDraws[i] = gameDraw
+	}
+	fmt.Println(gameDraws)
 	context.HTML(http.StatusOK, "dle.html", gin.H{
 		"Title":  "DLE",
 		"Mode":   playingMode,
