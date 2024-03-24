cbe8ef4ca35de184dfd13a3f01739c0f8d2374d3
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-03-24 20:28:35 -0300

    Add: basic hangman controller

--- cmd/runes/controller/mode-hangman.go
+++ cmd/runes/controller/mode-hangman.go
@@ -3,10 +3,22 @@ package controller
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strings"
 )
 
 func Hangman(context *gin.Context) {
+	letters := []string{}
+	for i := 'a'; i <= 'z'; i++ {
+		letters = append(letters, strings.ToUpper(string(i)))
+	}
+	c := "Viego"
+	var champion []string
+	for _, i := range c {
+		champion = append(champion, strings.ToUpper(string(i)))
+	}
 	context.HTML(http.StatusOK, "hangman.html", gin.H{
-		"Title": "Hangman",
+		"Title":    "Hangman",
+		"Champion": champion,
+		"Alphabet": letters,
 	})
 }
