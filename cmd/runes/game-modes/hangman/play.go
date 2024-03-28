9d7cc05a2524d7912269c90d0ab5b2d33f6d99c2
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-03-27 22:50:06 -0300

    Add: Hangman Draft

--- cmd/runes/game-modes/hangman/play.go
+++ cmd/runes/game-modes/hangman/play.go
@@ -2,13 +2,22 @@ package hangman
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/robfig/cron/v3"
+	"math/rand"
 	"net/http"
+	"runes/cmd/runes/database"
 	"strings"
 )
 
+var HangmanChampion = &c
+var c = database.CheckHangmanChampion()
+
+/*
+POST worker
+*/
 func PlayHangman(context *gin.Context) {
 	drawLetter := strings.ToUpper(context.PostForm("draw"))
-	c := strings.ToUpper("Viego")
+	c := strings.ToUpper(*HangmanChampion)
 	for index, letter := range c {
 		if drawLetter == string(letter) {
 			context.HTML(http.StatusOK, "hangman-dynamics.html", gin.H{
@@ -17,4 +26,22 @@ func PlayHangman(context *gin.Context) {
 			break
 		}
 	}
+}
+
+func DraftHangmanChampion() {
+	cronCycle := "0 0 * * *"
+	job := cron.New()
+	job.AddFunc(cronCycle, func() {
+		i := rand.Intn(len((*database.ChampionsList)))
+		championID := (*database.ChampionsList)[i].Champion.ID
+		database.SaveHangmanChampion(championID)
+		for i := range *database.ChampionsList {
+			if championID == (*database.ChampionsList)[i].Champion.ID {
+				c = (*database.ChampionsList)[i].Champion.Name
+				break
+			}
+		}
+	})
+	job.Start()
+	job.Run()
 }
