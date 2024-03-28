323108f56ace5c96cfa00a54574bb31892a6276b
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-03-27 22:49:44 -0300

    Add: new table variables

--- tools/envdata/env.go
+++ tools/envdata/env.go
@@ -7,21 +7,22 @@ import (
 )
 
 type EnvInfo struct {
-	DBHost          string
-	DBName          string
-	DBUser          string
-	DBPass          string
-	DBPort          string
-	SVPort          string
-	TBChampions     string
-	TBMayhem        string
-	TBMayhemDraws   string
-	TBGuess         string
-	TBGuessDraws    string
-	TBGuessChampion string
-	TBHangman       string
-	TBHangmanDraws  string
-	HeaderNode      string
+	DBHost            string
+	DBName            string
+	DBUser            string
+	DBPass            string
+	DBPort            string
+	SVPort            string
+	TBChampions       string
+	TBMayhem          string
+	TBMayhemDraws     string
+	TBGuess           string
+	TBGuessDraws      string
+	TBGuessChampion   string
+	TBHangman         string
+	TBHangmanDraws    string
+	TBHangmanChampion string
+	HeaderNode        string
 }
 
 var Env = envLoader()
@@ -32,21 +33,22 @@ func envLoader() *EnvInfo {
 		log.Fatal("envLoader() -> error while loading .envdata file")
 	}
 	myenv := &EnvInfo{
-		DBHost:          os.Getenv("DB_HOST"),
-		DBName:          os.Getenv("DB_NAME"),
-		DBUser:          os.Getenv("DB_USER"),
-		DBPass:          os.Getenv("DB_PASS"),
-		DBPort:          os.Getenv("DB_PORT"),
-		SVPort:          os.Getenv("SV_PORT"),
-		TBChampions:     os.Getenv("TB_CHAMPIONS"),
-		TBMayhem:        os.Getenv("TB_MAYHEM"),
-		TBMayhemDraws:   os.Getenv("TB_MAYHEM_DRAWS"),
-		TBGuess:         os.Getenv("TB_GUESS"),
-		TBGuessDraws:    os.Getenv("TB_GUESS_DRAWS"),
-		TBGuessChampion: os.Getenv("TB_GUESS_CHAMPION"),
-		TBHangman:       os.Getenv("TB_HANGMAN"),
-		TBHangmanDraws:  os.Getenv("TB_HANGMAN_DRAWS"),
-		HeaderNode:      os.Getenv("H_NODE"),
+		DBHost:            os.Getenv("DB_HOST"),
+		DBName:            os.Getenv("DB_NAME"),
+		DBUser:            os.Getenv("DB_USER"),
+		DBPass:            os.Getenv("DB_PASS"),
+		DBPort:            os.Getenv("DB_PORT"),
+		SVPort:            os.Getenv("SV_PORT"),
+		TBChampions:       os.Getenv("TB_CHAMPIONS"),
+		TBMayhem:          os.Getenv("TB_MAYHEM"),
+		TBMayhemDraws:     os.Getenv("TB_MAYHEM_DRAWS"),
+		TBGuess:           os.Getenv("TB_GUESS"),
+		TBGuessDraws:      os.Getenv("TB_GUESS_DRAWS"),
+		TBGuessChampion:   os.Getenv("TB_GUESS_CHAMPION"),
+		TBHangman:         os.Getenv("TB_HANGMAN"),
+		TBHangmanDraws:    os.Getenv("TB_HANGMAN_DRAWS"),
+		TBHangmanChampion: os.Getenv("TB_HANGMAN_CHAMPION"),
+		HeaderNode:        os.Getenv("H_NODE"),
 	}
 	return myenv
 }
